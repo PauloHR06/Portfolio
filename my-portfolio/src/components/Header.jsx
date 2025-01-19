34b7d296c341acb2f069768ed50ad48775820936
PauloHR06 <paulo.ribeiro@sou.inteli.edu.br>
2025-01-19 20:48:26 -0300

    Update Header.jsx

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
         <>
             <header className='text-white'>
                 <div className='container w-full flex justify-between p-6'>
-                    <div className='text-2xl font-bold'>Paulo Henrique Ribeiro</div>
+                    <div className='text-2xl font-bold font-mono'>&lt;&gt; Paulo Ribeiro &lt; /&gt;</div>
                     <div>
                         <nav>
                             <ul className='flex space-x-10'>
-                                <li><a href="#" className="hover:underline">Tecnologias</a></li>
-                                <li><a href="#" className="hover:underline">Projetos</a></li>
-                                <li><a href="#" className="hover:underline">Contato</a></li>
+                                <li><a href="#" className="hover:underline font-mono">Tecnologias</a></li>
+                                <li><a href="#" className="hover:underline font-mono">Projetos</a></li>
+                                <li><a href="#" className="hover:underline font-mono">Contato</a></li>
                             </ul>
                         </nav>
                     </div>
